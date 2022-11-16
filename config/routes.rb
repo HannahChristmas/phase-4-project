61fa8600dde4d95049950ca69ee60676e5111eee
HannahChristmas <hc109909@gmail.com>
2022-11-15 22:59:35 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :reviews
-  resources :recipes
-  resources :users
+  resources :reviews, only: [:create]
+  resources :recipes, only: [:index, :show, :create, :destroy]
+  resources :users, only: [:index, :show, :create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

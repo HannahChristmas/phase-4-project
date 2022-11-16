d3d1ac5a9f174917cee5e8220b16914effa35ff3
HannahChristmas <hc109909@gmail.com>
2022-11-15 23:01:56 -0500

    Update users_controller index and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def index 
+        render json: User.all.to_json, status: :ok
+    end
+
+    def show 
+        user = User.find(params[:id])
+        render json: user, status: :ok
+    end
 end

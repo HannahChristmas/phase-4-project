cfa8e33a80f54a8749049343bfa2f199457700e0
HannahChristmas <hc109909@gmail.com>
2022-11-15 22:34:13 -0500

    Initial setup of Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding data..."
+
+user1 = User.create(username: 'hannahchristmas', email: "hannah@gmail.com", password_digest: "hannah123")
+
+puts "✅ Done seeding"

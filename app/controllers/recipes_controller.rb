9855cc204b2336aff93eeefddf160fac2181573b
HannahChristmas <hc109909@gmail.com>
2022-11-16 10:58:05 -0500

    Update recipes_controller index and show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,10 @@
 class RecipesController < ApplicationController
+    def index 
+        render json: Recipe.all.to_json, status: :ok
+    end
+
+    def show 
+        user = Recipe.find(params[:id])
+        render json: user, status: :ok
+    end
 end
